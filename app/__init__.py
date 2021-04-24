edad22b29b03cb4bb3392a5543d5f2992b1820de
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-24 10:10:42 +0300

    setup flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,30 @@
 from flask import Flask
+from flask_login import LoginManager
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
+    # Initializing DATABASE.
+    db.init_app(app)
 
+    # Intializing Login Manager
+    login_manager.init_app(app)
     # Registering the blueprint
     from .main import main as main_blueprint
-
     app.register_blueprint(main_blueprint)
-
+    # authentication blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+    
     return app
