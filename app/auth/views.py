dc13e514f21f808a5eb0a738c80be4ba6526e6cc
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-04-25 20:59:02 +0300

    add log out route

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,3 +27,9 @@ def register():
         db.session.commit()
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
